99ddc2c0465104f238ce42a7454d3de7004087a4
pepe6500 <pepe6500@naver.com>
2025-03-31 10:26:29 +0900

    Project 기본 구조 생성

---
+++ popup.js
@@ -0,0 +1,16 @@
+﻿document.getElementById('getHTML').addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: getPageHTML,
+        }, displayResult);
+    });
+});
+
+function getPageHTML() {
+    return document.documentElement.outerHTML;
+}
+
+function displayResult(results) {
+    document.getElementById('result').innerText = results[0].result;
+}
