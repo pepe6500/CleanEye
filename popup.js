d4ad813cb3c5897107bcfbce21ccc2151cfa29ac
ljh15952 <ljh15952@gmail.com>
2025-05-19 22:10:09 +0900

    UI수정

--- popup.js
+++ popup.js
@@ -43,20 +43,20 @@ const original = '이건 진짜 나쁜놈이야!';
 const badWord = '나쁜놈';
 
 function updateExample() {
-    const method = censorSelect.value;
+    const method = parseInt(censorSelect.value, 10);
     let censored = '';
 
     switch (method) {
-        case 'asterisks':
+        case 1:
             censored = original.replace(badWord, '***');
             break;
-        case 'strikethrough':
+        case 2:
             censored = original.replace(badWord, `<s>${badWord}</s>`);
             break;
-        case 'AI':
+        case 3:
             censored = original.replace(badWord, '나쁜 사람');
             break;
-        case 'blur':
+        case 4:
             censored = original.replace(badWord, `<span class="blurred">${badWord}</span>`);
             break;
     }
@@ -65,7 +65,7 @@ function updateExample() {
 }
 
 censorSelect.addEventListener("change", () => {
-    const selectedMethod = censorSelect.value;
+    const selectedMethod = parseInt(censorSelect.value, 10);
     chrome.storage.local.set({ censorMethod: selectedMethod });
     updateExample();
 });
@@ -73,8 +73,8 @@ censorSelect.addEventListener("change", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get(["censorMethod"], (result) => {
-        const savedMethod = result.censorMethod ?? 'asterisks';
-        censorSelect.value = savedMethod;
+        const savedMethod = result.censorMethod ?? 1;
+        censorSelect.value = savedMethod.toString();
         updateExample(); 
     });
 });
@@ -84,14 +84,14 @@ document.addEventListener("DOMContentLoaded", () => {
 const imageFilterSelect = document.getElementById("imageFilterSelect");
 
 imageFilterSelect.addEventListener("change", () => {
-  const selectedImageFilter = imageFilterSelect.value;
+  const selectedImageFilter = parseInt(imageFilterSelect.value, 10);
   chrome.storage.local.set({ imageFilterMethod: selectedImageFilter });
 });
 
 // 팝업 열릴 때 불러오기
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["imageFilterMethod"], (result) => {
-    const savedImageMethod = result.imageFilterMethod ?? "mosaic";
-    imageFilterSelect.value = savedImageMethod;
+    const savedImageMethod = result.imageFilterMethod ?? 1;
+    imageFilterSelect.value = savedImageMethod.toString();
   });
 });
